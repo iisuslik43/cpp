7963324f7dc922efbfedb76bb1c880b76d903e34
iisuslik43 <al.s.fedotov1998@yandex.ru>
2016-10-07 00:24:44 +0400

    Delete str.h

--- str.h
+++
@@ -1,4 +0,0 @@
-char * strcat(char * destination, const char * source);
-int strlen(const char * str);
-char * strcpy(char * destination, const char * source);
-int strcmp(const char * str1, const char * str2);
