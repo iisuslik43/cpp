849b085d8971799dd6855adb068eefa3e91638ba
iisuslik43 <al.s.fedotov1998@yandex.ru>
2016-10-15 00:38:24 +0400

    Delete mergesort.h

--- mergesort.h
+++
@@ -1,5 +0,0 @@
-#include <stddef.h>
-int mergesort (void* base, size_t num, size_t size, int (*compare)(void*,void*));
-int compare_int(void* a, void* b);
-int compare_char(void* a, void* b);
-int compare_str(void * s1, void * s2);
